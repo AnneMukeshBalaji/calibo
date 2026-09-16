f3ddfde44d5051611cc13610ca8f4aa6d0879dce
AnneMukeshBalaji <mukimukku@gmail.com>
2026-09-16 13:20:43 +0530

    class end commit 16/09/2026

--- numpy/main.py
+++ numpy/main.py
@@ -21,6 +21,31 @@ print(ones)
 full_arr = np.full((2,3),7)
 print(full_arr)
 
-# Randome Numbers 
+# Random Numbers 
 random_arr = np.random.random((2,2))
 print(random_arr)
+
+# Identity Matrix 
+identity = np.eye(3)
+print(identity)
+
+# Array with range of values 
+range_arr = np.arange(0,10,2) # Start , Stop , Step 
+print(range_arr) 
+
+# Properties / Attributes in Numpy 
+# 1] Shape 
+print(arr_id.shape,identity.shape) # -> Rows,columns 
+# 2]Size
+print(identity.size) # -> 9 elements 
+# 3]Datatype
+print(identity.dtype) # -> int64 / float64
+# 4]Number of Dimenstions 
+print(identity.ndim) # -> 2 dimensional so 2 
+
+# Accessing Arrays in numpy
+array = np.array([1,2,3,4])
+print(array[-1])
+
+# Accessing Multi Dimensional Array 
+print(matrix[1,2]) # Row , column
